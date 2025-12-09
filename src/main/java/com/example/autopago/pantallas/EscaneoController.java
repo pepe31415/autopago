01edc2882599e55648efdab6d919c920015b7714
Jose Maria Castillo <josemariacastillo@MacBook-Pro-de-Jose.local>
2025-12-09 15:52:28 +0100

    UT6-sesion3 Añadido mecanismo de event listener

--- src/main/java/com/example/autopago/pantallas/EscaneoController.java
+++ src/main/java/com/example/autopago/pantallas/EscaneoController.java
@@ -2,12 +2,18 @@ package com.example.autopago.pantallas;
 
 import com.example.autopago.AutopagoApplication;
 import com.example.autopago.Navigator;
+import com.example.autopago.dispositivos.LectorCB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class EscaneoController {
+public class EscaneoController implements LectorCB.EventListener {
     private AutopagoApplication app = AutopagoApplication.getInstancia();
     private Navigator navigator = app. getNavigator();
+    private LectorCB lectorCB = app.getLectorCB();
+
+    public EscaneoController() {
+        lectorCB.connect(this);
+    }
     @FXML
     private void onCancelButtonClick(ActionEvent actionEvent) {
         System.out.println("Se ha pulsado boton cancelar de la escena escaneo.");
@@ -16,5 +22,10 @@ public class EscaneoController {
     @FXML
     private void onPagarButtonClick(ActionEvent actionEvent) {
         System.out.println("Se ha pulsado el boton pagar en la escena escaneo.");
+    }
+
+    @Override
+    public void alEscanearCB(String codigoBarras) {
+        System.out.println("EscaneoController recibió CB:" + codigoBarras);
     }
 }
